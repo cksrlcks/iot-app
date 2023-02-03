5dc5f71c28d776207480ae83dec06fb003cb3fcd
chanki <chanki.kim89@gmail.com>
2023-02-03 11:43:18 +0900

    브라우저 테스트 삭제

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import 'react-app-polyfill/ie11';
-import 'react-app-polyfill/stable';
+// import 'react-app-polyfill/ie11';
+// import 'react-app-polyfill/stable';
+// import 'core-js/stable';
+// import 'regenerator-runtime/runtime';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
